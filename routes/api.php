05797033df2c026e9c57618dd0b13781018c0595
TheAdeyemiOlayinka <olayinkaapps@gmail.com>
2025-01-01 13:07:48 +0100

    fix: user - 2

--- routes/api.php
+++ routes/api.php
@@ -95,7 +95,7 @@ Route::group(['middleware' => ['api', 'throttle:5,1']], function () {
     });
 });
 
-Route::middleware(['auth:api'])->get('/user', function (Request $request) {
+Route::middleware(['api', 'auth:api'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
