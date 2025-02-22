5105b841e1d09ba642dc673813e25901a1c0a175
TheAdeyemiOlayinka <olayinkaapps@gmail.com>
2025-02-22 21:17:22 +0100

    fix: issue deletion

---
+++ database/migrations/2025_02_22_201328_add_delete_constraints_to_issues_and_articles_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('issues', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('articles', callback: function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dropForeign(['issue_id']);
+            $table->foreign('issue_id')->references('id')->on('issues')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('issues', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dropForeign(['issue_id']);
+            $table->foreign('issue_id')->references('id')->on('issues');
+        });
+    }
+};
